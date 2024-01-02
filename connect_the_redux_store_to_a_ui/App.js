29489fdf8c42696c393da23c89fbb2e78eea72a4
Abida Nizam <83155476+abida128@users.noreply.github.com>
2024-01-02 12:06:53 +0000

    new commit

--- connect_the_redux_store_to_a_ui/App.js
+++ connect_the_redux_store_to_a_ui/App.js
@@ -1,19 +1,42 @@
+// import React from "react";
+// import { increment, decrement } from "./store";
+
+// function App({ state, dispatch}) {
+//   // Dispatch increment action
+//   const incrementerClicked = () => {
+
+//   }
+//   // Dispatch decrement action
+//   const decrementerClicked = () => {
+
+//   }
+
+//   return(
+//     <main>
+//       <p id='counter'>Waiting for current state.</p>
+//       <button id='incrementer' onClick={incrementerClicked}>+</button>
+//       <button id='decrementer' onClick={decrementerClicked}>-</button>
+//     </main>
+//   )
+// }
+
+// export default App;
 import React from "react";
 import { increment, decrement } from "./store";
 
 function App({ state, dispatch}) {
   // Dispatch increment action
   const incrementerClicked = () => {
-
+    dispatch(increment());
   }
   // Dispatch decrement action
   const decrementerClicked = () => {
-
+    dispatch(decrement());
   }
 
   return(
     <main>
-      <p id='counter'>Waiting for current state.</p>
+      <p id='counter'>{state}</p>
       <button id='incrementer' onClick={incrementerClicked}>+</button>
       <button id='decrementer' onClick={decrementerClicked}>-</button>
     </main>
@@ -21,3 +44,4 @@ function App({ state, dispatch}) {
 }
 
 export default App;
+
